5dc38d893b5263a07c3aefee548cdb2a4e1c725b
kacper <kacper.tarka00@gmail.com>
2022-12-25 00:50:49 +0100

    Main

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 from CMDApp import main_print
 from GUIApp import GUIApp
 import threading
+import argparse
 
 GAME = threading.Event()
 
 
 def main() -> None:
 
-    # uzycie GUI
-    # GUIApp()
-    # przyszlosciowe uzycie cmd
-    main_print(GAME)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--type', default="")
+    parser.add_argument('-t', default="")
+    args = parser.parse_args()
+    print(f"type = {type(args.type)}\nt = {type(args.t)}")
+    if args.type == "" and args.t == "":
+        # use cli
+        main_print(GAME)
+    elif args.t.lower() == 'gui' or args.type.lower() == 'gui':
+        #  use gui
+        GUIApp()
+    else:
+        print("Sorry I don't understand :/")
 
 
 if __name__ == "__main__":
